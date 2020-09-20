d382da57bfb2b9e7e600907a99679aef7c9ab66e
stevemussmann <smussmann@gmail.com>
2020-09-20 13:59:47 -0600

    now runs primer3

--- primer3.py
+++ primer3.py
@@ -1,17 +1,35 @@
 from __future__ import print_function
 
 import os.path
+import subprocess
+import sys
 
 class Primer3():
 	'Class for executing primer3'
 
 	def __init__(self, fn):
-		#self.fn=fn
-		#self.procFile()
+		# make directories
 		self.cwd=os.getcwd()
 		self.fas=os.path.join(self.cwd, "muscle_aligned_consensus", fn)
+		self.p3inDir=os.path.join(self.cwd, "p3in")
+		self.p3outDir=os.path.join(self.cwd, "p3out")
+		if(os.path.isdir(self.p3inDir) != True):
+			os.mkdir(self.p3inDir)
+		if(os.path.isdir(self.p3outDir) != True):
+			os.mkdir(self.p3outDir)
+		
+		# parse file
 		seqinfo = self.parseFile()
+
+		# make p3in file
 		self.makeFile(seqinfo["ID"], seqinfo["SEQUENCE"])
+
+		# run primer3_core
+		comm = self.makeCommand(fn)
+		#print(comm)
+
+		print("Finding primers for", fn)
+		self.runProgram(comm)
 
 	def makeFile(self, name, seq):
 		fn=name + ".p3in.txt"
@@ -48,11 +66,11 @@ class Primer3():
 		f.write("\n")
 		f.write("PRIMER_GC_CLAMP=2")
 		f.write("\n")
-		f.write("PRIMER_MIN_TM=57")
+		f.write("PRIMER_MIN_TM=58")
 		f.write("\n")
-		f.write("PRIMER_OPT_TM=60")
+		f.write("PRIMER_OPT_TM=61")
 		f.write("\n")
-		f.write("PRIMER_MAX_TM=65")
+		f.write("PRIMER_MAX_TM=64")
 		f.write("\n")
 		f.write("PRIMER_PAIR_MAX_DIFF_TM=2")
 		f.write("\n")
@@ -64,7 +82,7 @@ class Primer3():
 		f.write("\n")
 		f.write("P3_FILE_FLAG=1")
 		f.write("\n")
-		f.write("SEQUENCE_INTERNAL_EXCLUDED_REGION=151,3") #add "start coord, length"
+		f.write("SEQUENCE_INTERNAL_EXCLUDED_REGION=150,3") #add "start coord, length"
 		f.write("\n")
 		f.write("PRIMER_OPT_GC_PERCENT=50")
 		f.write("\n")
@@ -91,8 +109,31 @@ class Primer3():
 		seqinfo["ID"] = data.pop(0).replace(">", "")
 		seqinfo["SEQUENCE"] = "".join(data).upper()
 
-		print(seqinfo["ID"])
-		print(seqinfo["SEQUENCE"])
+		#print(seqinfo["ID"])
+		#print(seqinfo["SEQUENCE"])
 
 		return seqinfo
 
+	def makeCommand(self, fn):
+		outfn = fn + ".p3out.txt"
+		infn = fn + ".p3in.txt"
+		out=os.path.join(self.p3outDir, outfn)
+		indir=os.path.join(self.p3inDir, infn)
+		string = "primer3_core -format_output -output=" + out + " < " + indir
+		return string
+
+	def runProgram(self,string):
+		try:
+			process = subprocess.Popen(string, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+			output, err = process.communicate()
+			print(err)
+			if process.returncode != 0:
+				print("Non-zero exit status:")
+				print(process.returncode)
+				raise SystemExit
+		except(KeyboardInterrupt, SystemExit):
+			raise
+		except:
+			print("Unexpected error:")
+			print(sys.exec_info())
+			raise SystemExit
